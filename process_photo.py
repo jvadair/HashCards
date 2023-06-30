b6909aeb083191ca26333545ed046fb3a6346258
jvadair <dev@jvadair.com>
2023-06-29 20:16:53 -0400

    Fix photo compression and add run case

--- process_photo.py
+++ process_photo.py
@@ -24,7 +24,16 @@ def process_photo(filename, max_size=100):
     :return:
     """
     new_filename = filename.rsplit('.', maxsplit=1)[0] + '.jpg'
-    command = f"convert {filename} -define jpeg:extent={max_size}kb birdtest-compressed.jpg"
+    command = f"convert {filename} -define jpeg:extent={max_size}kb {new_filename}"
     os.system(command)
     if new_filename != filename:
         os.remove(filename)
+
+
+if __name__ == "__main__":
+    to_compress = [
+        f"static/images/pfp/{filename}" for filename in os.listdir("static/images/pfp")
+    ]
+    for filename in to_compress:
+        if validate_photo(filename):
+            process_photo(filename, max_size=50)
