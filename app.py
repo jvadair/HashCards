5fee38135f168ac41d7e841290113c6008a03c92
jvadair <dev@jvadair.com>
2023-08-01 23:19:28 -0400

    Fix location of email within google response token

--- app.py
+++ app.py
@@ -400,7 +400,7 @@ def google_auth():
     # profile = resp.json()
     # print("Google login:", token['user_id'])
     # print("Google token:", token)
-    email = token['email']
+    email = token['userinfo']['email']
     username = email.split('@gmail.com')  # This will look weird for non-gmails, but oh well!
     was_created = r_api.handle_social_login(username, 'google', session)
     user_db = get_user_db(session['id'])
